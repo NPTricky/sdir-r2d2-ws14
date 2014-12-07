5a534b85771631f013e0b99aa4cfcb8d2b236cb5
Tim Benedict Jagla <tim.jagla@gmail.com>
2014-12-07 09:27:03 +0100

    hopefully everything required by eclipse, tried to ignore every kind of local and irrelevant files

---
+++ workspace/Kinematics/src/Kinematics/__init__.py
@@ -0,0 +1,99 @@
+from openravepy import *
+import Kinematics as kin
+import MotionFunctions as mf
+import numpy as np
+import sys
+import socket
+
+
+# handles the data transfer between openrave (server) and the GUI (client)
+def dataTransfer():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('localhost', 54321))
+    s.listen(1)
+    
+    try:
+        while True:
+            komm, addr = s.accept()     
+            while True:
+                data = komm.recv(2048)
+                if not data:
+                    komm.close()
+                    break
+                # parse received data and get the string that should be sent back to the GUI
+                send = handleData(data)
+                
+                # send new informations to the GUI for updating purposes
+                komm.sendto(send, addr)
+    finally:
+        s.close()
+
+
+
+# handles the data received from the GUI and sets up data for sending
+def handleData(data):
+    # split data string
+    data_arr = data.split("#")
+    
+    # check if GUI requests the current robot axis values as well as current orientation and position 
+    if data_arr[0] == 'GET':
+        # prefix for parsing
+        prefix = "VAL#"
+        # get Axis values
+        axis_arr = robot.GetDOFValues()
+        # convert to string
+        axis_values = str(axis_arr[0])+";"+str(axis_arr[1])+";"+str(axis_arr[2])+";"+str(axis_arr[3])+";"+str(axis_arr[4])+";"+str(axis_arr[5])+'#'
+        # adding dummy values for orientation and position (you need to compute the values)
+        cart_values = "0;0;0;0;0;0"
+        return prefix+axis_values+cart_values
+    
+    # check if the robot should be moved 
+    elif data_arr[0] == 'MOV':
+        # get values
+        values = data_arr[1].split(';')
+        # convert from string to float and save in numpy array
+        target = np.array([float(values[0]), float(values[1]), float(values[2]), float(values[3]), float(values[4]), float(values[5])])
+                
+        # get the motion type
+        motion_type = data_arr[2]
+        
+        # get trajectory
+        trajectory = mf.PTPtoConfiguration(robot.GetDOFValues(), target, motion_type)
+        # move robot
+        mf.Move(robot, trajectory)
+        
+        # send new information about the robot's axis values, position and orientation to the GUI for updating purpose
+        # prefix for parsing
+        prefix = "VAL#"
+        # get Axis values
+        axis_arr = robot.GetDOFValues()
+        # convert to string
+        axis_values = str(axis_arr[0])+";"+str(axis_arr[1])+";"+str(axis_arr[2])+";"+str(axis_arr[3])+";"+str(axis_arr[4])+";"+str(axis_arr[5])+'#'
+        # adding dummy values for orientation and position (you need to compute the values)
+        cart_values = "0;0;0;0;0;0"     
+        return prefix+axis_values+cart_values
+    
+    # check if inverse kinematics should be calculated
+    if data_arr[0] == "CAL":
+        # get string with values
+        values = data_arr[1].split(';')
+        
+        # calculate inverse kinematic solution
+        
+        # send the (multiple) solutions to the GUI
+        # prefix for parsing
+        prefix = "INK#"
+        # adding dummy values (you need to replace them with the solutions)
+        ik_values = "0;0;0;0;0;0"
+        return prefix+ik_values
+    
+    
+if __name__ == "__main__":
+    # setting up the operave environment
+    env = Environment() # create openrave environment
+    env.SetViewer('qtcoin') # attach viewer (optional)
+    env.Load('../../MyData/MyEnvironment/MyEnv.xml') # load a simple scene
+    robot = env.GetRobots()[0] # get the first robot
+    dataTransfer()
+    
+    
