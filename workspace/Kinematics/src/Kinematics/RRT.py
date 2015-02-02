4211b703b9970549b4e7dd54ee89a936062b4642
Tim Benedict Jagla <tim.jagla@gmail.com>
2015-02-02 12:16:50 +0100

    code clean-up (deleted deprecated functions & minor refactoring)

--- workspace/Kinematics/src/Kinematics/RRT.py
+++ workspace/Kinematics/src/Kinematics/RRT.py
@@ -28,23 +28,6 @@ _RRT_PRECISION_NEIGHBOR = 5
 _PLOT_HISTORY = False
 
 
-# input:
-# - state:
-# return the configuration of the given state
-def get_cfg(state):
-    assert(len(state) == _STATE_LEN)
-    return state[:_CFG_LEN]
-
-
-
-# input:
-# - cfg:
-# create a state from a given configuration and velocity
-def create_state(cfg):
-    assert(len(cfg) == _CFG_LEN)
-    return np.array(cfg)
-
-
 
 # green = high z coordinate
 # blue = low z coordinate
@@ -66,7 +49,7 @@ def attributes_and_spatial_layout(graph):
     layout = []
     for vertex in graph.vs:
         vertex_pose = kin.get_pose_from(kin.forward(vertex["configuration"]))
-        vertex["vertex_size"] = map_interval(0.567, 3.567, 10.0, 40.0, vertex_pose[2])
+        vertex["vertex_size"] = map_interval(0.567, 3.567, 10.0, 30.0, vertex_pose[2])
         vertex["vertex_color"] = heatmap_rgb(vertex_pose[2])
         layout.append((vertex_pose[0],vertex_pose[1]))
     return layout
@@ -126,31 +109,6 @@ def draw_graph(graph, env):
 
 
 # input:
-# - cfg_init:
-# - cfg_goal:
-# - t:
-# interpolate between two configurations with parameter t
-def lerp(cfg_init, cfg_goal, t):
-    assert(0 <= t and t <= 1) # try not to extrapolate
-    return (1 - t) * cfg_init + t * cfg_goal
-
-
-
-# input:
-# - cfg_init:
-# - cfg_goal:
-# - precision:
-# apply the precision parameters
-def lerp_range(cfg_init, cfg_goal, precision):
-    # 0.0 and 1.6 are experimentally proven to describe the average norm of an
-    # average motion
-    max_i = int(map_interval(0.0, 1.6, 1.0, precision, np.linalg.norm(cfg_goal - cfg_init)))
-    factor = 1 / max_i
-    return max_i, factor
-
-
-
-# input:
 # - from_min:
 # - from_max:
 # - to_min:
@@ -190,19 +148,9 @@ def is_valid(robot, configuration):
 
 # input:
 # - robot:
-# - cfg_init:
-# - cfg_goal:
+# - trajectory;
 # output:
-# - if the path is collision free
-def is_valid_path(robot, cfg_init, cfg_goal):
-    max_i, factor = lerp_range(cfg_init, cfg_goal, _RRT_PRECISION_COLLISION)
-    for i in xrange(1,max_i-1):
-        valid = is_valid(robot, lerp(cfg_init, cfg_goal, i * factor))
-        if not valid:
-            i = 0
-            break
-    return not i == 0
-
+# - whether the trajectory is collision free
 def is_valid_trajectory(robot, trajectory):
     for i in xrange(1, len(trajectory) - 1):
         valid = is_valid(robot, trajectory[i])
@@ -214,18 +162,9 @@ def is_valid_trajectory(robot, trajectory):
 
 # input:
 # - robot:
-# - cfg_init:
-# - cfg_goal:
+# - trajectory:
 # output:
-# - possible configuration
-def make_valid_path(robot, cfg_init, cfg_goal):
-    max_i, factor = lerp_range(cfg_init, cfg_goal, _RRT_PRECISION_COLLISION)
-    for i in xrange(1,max_i-1):
-        valid = is_valid(robot, lerp(cfg_init, cfg_goal, i * factor))
-        if not valid:
-            return lerp(cfg_init, cfg_goal, (i - 1) * factor)
-    return cfg_goal
-
+# - a valid trajectory over its entire range
 def make_valid_trajectory(robot, trajectory):
     for i in xrange(1, len(trajectory) - 1):
         valid = is_valid(robot, trajectory[i])
@@ -238,12 +177,13 @@ def make_valid_trajectory(robot, trajectory):
 # input:
 # - robot:
 # output:
-# - random state in the configuration space (velocity of 0.0)
+# - random state in the configuration space
 def generate_random_state(robot):
     lower,upper = robot.GetDOFLimits()
     angular_limits_difference = upper - lower
     configuration_random = lower + np.random.sample(len(lower)) * angular_limits_difference
-    return create_state(configuration_random)
+    return configuration_random
+
 
 
 # input:
@@ -253,6 +193,7 @@ def generate_random_state(robot):
 # - the nearest configuration
 # - the index of the vertex of the nearest configuration
 # find the nearest neighbor of a (random) state about to be inserted into the graph
+# and insert a new state into the graph if required
 def find_nearest_neighbor_new(graph, state_goal):
     # container for the resulting configurations by interpolation of existing edges
     edge_interpolation = np.empty([0,6])
@@ -267,7 +208,7 @@ def find_nearest_neighbor_new(graph, state_goal):
     search_container = np.concatenate((graph.vs["configuration"],edge_interpolation))
     search_structure = spatial.KDTree(search_container)
     # search for smallest distance between configurations (KNN with k = 1)
-    distance, search_idx = search_structure.query(get_cfg(state_goal), 1)
+    distance, search_idx = search_structure.query(state_goal, 1)
     if search_idx < interpolation_idx_offset:
         # state is already in the graph
         state_near = graph.vs["configuration"][search_idx]
@@ -275,7 +216,7 @@ def find_nearest_neighbor_new(graph, state_goal):
     else:
         # state is not already in the graph
         # guaranteed to be collision free as it was interpolated from tested trajectory
-        state_near = create_state(search_container[search_idx])
+        state_near = search_container[search_idx]
         state_near_idx = insert_state(state_near, graph)
         # retrieve the edge about to split
         edge_split_idx = int(edge_idx_list[search_idx-interpolation_idx_offset])
@@ -296,80 +237,13 @@ def find_nearest_neighbor_new(graph, state_goal):
 
 # input:
 # - robot:
-# - state_goal:
-# - graph:
-# output:
-# - the nearest configuration
-# - the index of the vertex of the nearest configuration
-# find the nearest neighbor of a (random) state about to be inserted into the graph
-def find_nearest_neighbor(robot, state_goal, graph):
-    # container for the resulting configurations by interpolation of existing edges
-    edge_interpolation = []
-    # keep track of edge index
-    edge_idx_list = []
-    for a,b in graph.get_edgelist():
-        cfg_init = graph.vs["configuration"][a]
-        cfg_goal = graph.vs["configuration"][b]
-        max_i, factor = lerp_range(cfg_init, cfg_goal, _RRT_PRECISION_NEIGHBOR)
-        for i in xrange(1,max_i-1):
-            edge_interpolation.append(lerp(cfg_init, cfg_goal, i * factor))
-            edge_idx_list.append(a)
-    
-    interpolation_idx_offset = len(graph.vs["configuration"])
-    search_container = graph.vs["configuration"] + edge_interpolation
-    search_structure = spatial.KDTree(search_container)
-    # search for smallest (euclidean) distance between configurations (KNN with k = 1)
-    distance, search_idx = search_structure.query(get_cfg(state_goal), 1);
-    if search_idx < interpolation_idx_offset:
-        # state is already in the graph
-        state_near = create_state(graph.vs["configuration"][search_idx])
-        state_near_idx = search_idx
-    else:
-        # state is not already in the graph
-        state_near_cfg = make_valid_path(robot, search_container[search_idx], get_cfg(state_goal))
-        state_near = create_state(state_near_cfg)
-        state_near_idx = insert_state(state_near, graph)
-        insert_edge(edge_idx_list[search_idx-interpolation_idx_offset], state_near_idx, graph)
-    return state_near,state_near_idx
-
-
-
-# no heuristics or rules for a generated state
-def generate_state_naive(robot, state_init, state_goal, delta_time):
-    return state_goal
-
-
-
-# input:
-# - robot:
 # - state_init:
 # - state_goal:
-# - delta_time:
-# output:
-# - new state generated by the given state_init and delta_time
-def generate_state(robot, state_init, state_goal, delta_time):
-    cfg_init = get_cfg(state_init)
-    cfg_goal = get_cfg(state_goal)
-    # calculate difference & sign of the difference
-    difference = mf.difference_rel(cfg_init, cfg_goal)
-    sign = np.sign(difference)
-    # calculate limits (without signs)
-    v_limit = np.sqrt(np.fabs(difference) * robot.GetDOFAccelerationLimits())
-    velocity_limit = np.minimum(v_limit, robot.GetDOFVelocityLimits())
-    times_acc = velocity_limit / robot.GetDOFAccelerationLimits()
-    # calculate and apply delta
-    delta = velocity_limit * (delta_time * 0.5 * times_acc)
-    cfg_new = limit_cfg(robot, cfg_init + (delta * sign))
-    return create_state(cfg_new)
-
-# input:
-# - robot:
-# - state_init:
-# - state_goal:
+# - motiontype:
 # - delta_time:
 # output:
 # - new state generated by the given states and delta_time
-# - collision-free
+# - the valid, collision-free trajectory
 def generate_state_new(robot, state_init, state_goal, motiontype, delta_time = sys.maxint):
     # calculate the trajectory from the start configuration to the target configuration
     configuration = np.array((state_init, state_goal))
@@ -380,7 +254,7 @@ def generate_state_new(robot, state_init, state_goal, motiontype, delta_time = s
     # maximum time step count overall
     # do not append the configuration for state_goal (len(trajectory) - 2)
     time_steps_max = min(len(trajectory) - 2, time_steps_limit)
-    step = int(max(1, np.linalg.norm(get_cfg(state_goal) - get_cfg(state_init))))
+    step = int(max(1, np.linalg.norm(state_goal - state_init)))
     
     # create a smaller trajectory to save it into the graph structure
     # do not append the configuration for state_init (xrange(1,...))
@@ -398,16 +272,6 @@ def generate_state_new(robot, state_init, state_goal, motiontype, delta_time = s
 
 
 # input:
-# - robot;
-# - cfg:
-# limit the configuration to the given lower and upper limits and return the result
-def limit_cfg(robot, cfg):
-    lower,upper = robot.GetDOFLimits()
-    return np.clip(cfg, lower, upper)
-
-
-
-# input:
 # - state: state to insert into the graph
 # - graph: the corresponding graph
 # output:
@@ -416,7 +280,7 @@ def insert_state(state, graph):
     graph.add_vertex()
     vertex = graph.vs[graph.vcount()-1]
     vertex["name"] = vertex.index
-    vertex["configuration"] = get_cfg(state)
+    vertex["configuration"] = state
     return vertex.index
 
 
@@ -445,7 +309,7 @@ def generate_rt(robot, motiontype, delta_time, iterations):
     _CFG_LEN = robot.GetDOF()
     
     # create initial & goal state for the rrt algorithm
-    state_init = create_state(robot.GetDOFValues())
+    state_init = robot.GetDOFValues()
     _STATE_LEN = len(state_init)
     
     # create graph structure with initial state
@@ -462,7 +326,7 @@ def generate_rt(robot, motiontype, delta_time, iterations):
         state_new_idx = insert_state(state_new, g)
         # add edge and assign distance as an attribute
         edge_idx = insert_edge(state_near_idx, state_new_idx, g)
-        g.es[edge_idx]["weight"] = np.linalg.norm(get_cfg(state_new) - get_cfg(state_near))
+        g.es[edge_idx]["weight"] = np.linalg.norm(state_new - state_near)
         g.es[edge_idx]["trajectory"] = trajectory
         if _PLOT_HISTORY: plot_igraph(g, i)
         
@@ -478,9 +342,9 @@ def node_to_cfg(graph, index_path):
         cfg = graph.vs["configuration"][index_path[i]]
         cfg_path.append(cfg)
     return cfg_path
-    
-    
-    
+
+
+
 # input;
 # - robot:
 # - goal_cfg:
